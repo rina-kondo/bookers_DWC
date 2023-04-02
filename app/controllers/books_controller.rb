00e5f9990f158bb4e229f630956469b33d569b46
rina-kondo <122541296+rina-kondo@users.noreply.github.com>
2023-04-02 14:47:50 +0000

    updated all

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,21 @@
 class BooksController < ApplicationController
   def new
-    @book = Book.new
   end
 
   def create
     @book = Book.new(book_params)
     if @book.save
-      flash[:notice] =　"Book was successfully created."
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
-      render books_path
+      render :index
     end
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -31,11 +31,10 @@ class BooksController < ApplicationController
     @book.update(book_params)
 
     if @book.save
-      flash[:notice] =　"Book was successfully updated."
+      flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id)
     else
-      @books = Book.all
-      render books_path
+      render :edit
     end
   end
 
